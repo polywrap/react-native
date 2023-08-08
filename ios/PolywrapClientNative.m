bc6ca74a53149dbaf307853da73072390678a3f5
Cesar <cesarbrazon10@gmail.com>
2023-08-08 16:29:41 +0200

    feat: swift module works in react native

--- ios/PolywrapClientNative.m
+++ ios/PolywrapClientNative.m
@@ -1,13 +1,17 @@
 #import <React/RCTBridgeModule.h>
 
-@interface RCT_EXTERN_MODULE(PolywrapClientNative, NSObject)
+@interface RCT_EXTERN_REMAP_MODULE(PolywrapClient, PolywrapClientNative, NSObject)
 
 RCT_EXTERN_METHOD(invokeRaw:(NSString *)uri
-                  withMethod:(NSString *)method
-                  withArgs:(NSArray<NSNumber *> *)args
-                  withEnv:(NSArray<NSNumber *> *)env
-                  withResolver:(RCTPromiseResolveBlock)resolve
-                  withRejecter:(RCTPromiseRejectBlock)reject)
+                  method:(NSString *)method
+                  args:(NSArray<NSNumber *> *)args
+                  env:(NSArray<NSNumber *> *)env
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(configureAndBuild:(NSDictionary *)clientConfig
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject)
 
 + (BOOL)requiresMainQueueSetup
 {
--- ios/PolywrapClientNative.swift
+++ ios/PolywrapClientNative.swift
@@ -2,7 +2,7 @@ import PolywrapClient
 import Foundation
 
 @objc(PolywrapClientNative)
-class PolywrapClientNative: NSObject, RCTBridgeModule {
+class PolywrapClientNative: NSObject {
 
     static func moduleName() -> String! {
         return "PolywrapClientNative"
@@ -14,7 +14,7 @@ class PolywrapClientNative: NSObject, RCTBridgeModule {
 
     private var client: PolywrapClient?
 
-    @objc
+    @objc(invokeRaw:method:args:env:resolve:reject:)
     func invokeRaw(
         _ uri: String,
         method: String,
@@ -24,28 +24,104 @@ class PolywrapClientNative: NSObject, RCTBridgeModule {
         reject: @escaping RCTPromiseRejectBlock
     ) {
 
-        if let client = self.client {
-            client
-        } else {
-            let builder = BuilderConfig().build()
-            builder.build()
-        }
-        // Your implementation goes here
-
-        guard let uriSanitized = try Uri(uri) else {
-            reject("Uri sanitization failed")
+        // Ensure the client is instantiated
+        if self.client == nil {
+            let builder = BuilderConfig()
+            self.client = builder.build()
         }
 
+        guard let clientUnwrapped = self.client else {
+            reject("CLIENT_ERROR", "Failed to initialize client", nil)
+            return
+        }
+
+        // Check if the URI is okay
+        guard let uriSanitized = try? Uri(uri) else {
+            reject("URI_ERROR", "Uri sanitization failed", nil)
+            return
+        }
+
+        // Call client.invokeRaw with the sanitized URI
         do {
-            let result = try client.invokeRaw(
+            let result = try clientUnwrapped.invokeRaw(
                 uri: uriSanitized,
                 method: method,
                 args: args,
                 env: env
             )
             resolve(result)
+        } catch let error as NSError {
+            reject("INVOKE_ERROR", "Failed to invoke raw method", error)
+        }
+    }
+
+    @objc(configureAndBuild:resolve:reject:)
+    func configureAndBuild(
+        _ clientConfig: NSDictionary,
+        resolve: @escaping RCTPromiseResolveBlock,
+        reject: @escaping RCTPromiseRejectBlock
+    ) {
+        do {
+            var builder = BuilderConfig()
+            // Handling envs
+            if let envs = clientConfig["envs"] as? NSDictionary {
+                for (key, value) in envs {
+                    guard let envName = key as? String, 
+                        let envBytes = value as? [UInt8] else { continue }
+
+                    guard let uri = try? Uri(envName) else {
+                        reject("CONFIGURE_AND_BUILD_ERROR", "Env uri sanitization failed", nil)
+                        return
+                    }
+
+                    builder.ffi.addEnv(uri: uri.ffi, env: envBytes)
+                }
+            }
+
+            // Handling interfaces
+            if let interfaces = clientConfig["interfaces"] as? NSDictionary {
+                for (key, value) in interfaces {
+                    guard let interfaceName = key as? String,
+                        let implementationsArray = value as? [String] else { continue }
+                    
+                    let implementations = implementationsArray.compactMap { try? Uri($0) }
+                    guard implementations.count == implementationsArray.count else {
+                        reject("IMPLEMENTATION_URI_ERROR", "Implementation uri sanitization failed", nil)
+                        return
+                    }
+
+                    guard let interface = try? Uri(interfaceName) else {
+                        reject("CONFIGURE_AND_BUILD_ERROR", "Interface uri sanitization failed", nil)
+                        return
+                    }
+                    builder = builder.addInterfaceImplementations(interface, implementations)
+                }
+            }
+
+            // Handling redirects
+            if let redirects = clientConfig["redirects"] as? NSDictionary {
+                for (key, value) in redirects {
+                    guard let redirectFrom = key as? String,
+                        let redirectTo = value as? String else { continue }
+
+                    
+                    guard let from = try? Uri(redirectFrom) else {
+                        reject("CONFIGURE_AND_BUILD_ERROR", "Redirect from uri sanitization failed", nil)
+                        return
+                    }
+                    
+                    guard let to = try? Uri(redirectTo) else {
+                        reject("CONFIGURE_AND_BUILD_ERROR", "Redirect to uri sanitization failed", nil)
+                        return
+                    }
+                    builder = builder.addRedirect(from, to)
+                }
+            }
+            
+            self.client = builder.addSystemDefault().build()
+            resolve(true)
         } catch {
-            reject(error)
+            reject("CONFIGURE_AND_BUILD_ERROR", "Failed to configure and build client", error)
         }
     }
 }
